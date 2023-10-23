678343b467854e7c577c0c2508c1eb1ca6c51fa4
Silverfoot42 <elliottgc@gmail.com>
2023-10-23 03:24:47 -0400

    added files

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your aplication?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Enter the description of your application:',
+    },
+    {
+      type: 'input',
+      name: 'instructions',
+      message: 'Enter the installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'information',
+      message: 'Enter the usage information:',
+    },
+    {
+      type: 'input',
+      name: 'guidelines',
+      message: 'Enter the contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'test',
+      message: 'Enter the test instructions:',
+    },
+  ])
+  .then((answers) => {
+    generateMarkdown();
 
+    
+  });
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(filename, data) {
+
+}
 
 // TODO: Create a function to initialize app
 function init() {}
